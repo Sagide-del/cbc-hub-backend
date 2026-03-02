a0eae0c1af30d1129d2ad3a5c7ee4d32b8d4f67e
sirdide-cloud <sagidefrankline@gmail.com>
2026-03-02 06:02:59 +0300

    Simplify Vercel configuration

--- vercel-build.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-console.log('?? Running Strapi build for Vercel...');
-execSync('npm run build', { stdio: 'inherit' });
-console.log('? Build complete');
