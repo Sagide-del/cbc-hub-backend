09b85035a0e73072348c3f9119f23ddfd7b08acd
sirdide-cloud <sagidefrankline@gmail.com>
2026-03-02 05:04:33 +0300

    Initial Strapi backend for CBC Hub

---
+++ src/api/diy-project/controllers/diy-project.ts
@@ -0,0 +1,7 @@
+/**
+ * diy-project controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::diy-project.diy-project');
---
+++ src/api/diy-project/routes/diy-project.ts
@@ -0,0 +1,7 @@
+/**
+ * diy-project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::diy-project.diy-project');
