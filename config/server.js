161bda4791dc25e984669d15048db311ddbea19f
sirdide-cloud <sagidefrankline@gmail.com>
2026-03-02 15:50:15 +0300

    Clean config: remove TypeScript files and hardcode secrets

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: '0.0.0.0',
+  port: 1337,
   app: {
-    keys: env.array('APP_KEYS', ['key1', 'key2', 'key3', 'key4']),
+    keys: ['Xk9pLm2nRq4sTv8wYz3bCd6fGh1jKl5M', 'Ab7cDe9fGh2iJk4lMn6oPq8rSt0uVw1x', 'Cd8eFg1hIj2kLm3nOp4qRs5tUv6wXy7z', 'De9fGh2iJk4lMn6oPq8rSt0uVw1xYz3b'],
   },
 });
